d6a31d89668f16b8911e7d3ef0bf02ac16deb1e2
wwoopp <wwoopp@wwoopp.com>
2022-04-10 05:47:45 +0900

    초기 페이지 만들어보기

---
+++ src/main/java/com/wwoopp/firstspringboot/IndexController.java
@@ -0,0 +1,14 @@
+package com.wwoopp.firstspringboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping
+    String index() {
+        return "index";
+    }
+
+}
