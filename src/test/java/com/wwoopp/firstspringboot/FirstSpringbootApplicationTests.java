f31e2df67a993d9df17b9dde4044529c362dbb94
wwoopp <wwoopp@wwoopp.com>
2022-04-10 05:12:05 +0900

    첫 스프링 부트 프로젝트 시작

---
+++ src/test/java/com/wwoopp/firstspringboot/FirstSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwoopp.firstspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
